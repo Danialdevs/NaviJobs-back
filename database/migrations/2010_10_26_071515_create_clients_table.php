199929a11c5a17142a9f42fdd8d45fdd8594158e
Zakharrrrr <storchakz@list.ru>
2024-06-26 12:17:20 +0500

    migration

--- database/migrations/2010_10_26_071515_create_clients_table.php
+++ database/migrations/2010_10_26_071515_create_clients_table.php
@@ -4,22 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateClientsTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('phone_number');
-            $table->string('tg_id');
-            $table->foreignId('company_id')->constrained('companies');
             $table->timestamps();
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::dropIfExists('clients');
     }
-}
+};
