b8e679cc13fe5165aca74103b44415188d4864d7
Zakharrrrr <storchakz@list.ru>
2024-06-26 12:20:56 +0500

    migration

--- database/migrations/2010_09_26_070351_create_services_table.php
+++ database/migrations/2010_09_26_070351_create_services_table.php
@@ -4,22 +4,28 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateClientsTable extends Migration
+class CreateUsersTable extends Migration
 {
     public function up()
     {
-        Schema::create('clients', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('office_id')->constrained('company_offices');
+            $table->string('surname');
             $table->string('name');
-            $table->string('phone_number');
-            $table->string('tg_id');
-            $table->foreignId('company_id')->constrained('companies');
+            $table->string('last_name');
+            $table->string('sex');
+            $table->date('data_birthday');
+            $table->string('role');
+            $table->string('avatar')->nullable();
+            $table->string('email')->unique();
+            $table->string('password');
             $table->timestamps();
         });
     }
 
     public function down()
     {
-        Schema::dropIfExists('clients');
+        Schema::dropIfExists('users');
     }
 }
