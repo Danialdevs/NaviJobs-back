68eb95ca029683246c0485dade411de1e2645991
Zakharrrrr <storchakz@list.ru>
2024-07-03 02:09:05 +0500

    create autorisation + changes

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,14 +3,34 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Api\User\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response as HttpResponse; // Используем алиас для разрешения констант
 
 class UserController extends Controller
 {
-    public function Register(request $request){
-        return(view('register'));
-    }
-    public function Login(request $request){
-        return(view('login'));
+    public function login(LoginRequest $request): JsonResponse
+    {
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        /** @var User|null $user */
+        $user = Auth::guard('web')->user();
+
+        if (!$user || !Auth::guard('web')->attempt(['email' => $email, 'password' => $password])) {
+            return response()->json([
+                'message' => 'Неверный логин или пароль',
+            ], HttpResponse::HTTP_UNAUTHORIZED);
+        }
+
+        $token = $user->createToken('login');
+
+        $user->update(['api_token' => $token->plainTextToken]);
+
+        return response()->json([
+            'message' => 'Успешный вход в систему',
+        ], HttpResponse::HTTP_OK);
     }
 }
