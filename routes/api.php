b8dae49df58bc10849abc9a23a0889ba0d9f7028
Haten00 <johanworkofficial@mail.ru>
2024-07-04 10:00:26 +0600

    add changes api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,10 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
-Route::post('/login', function (Request $request){
-    $user = Auth::attempt($request);
-    return $user->createToken("test")->plainTextToken;
-
-
 });
