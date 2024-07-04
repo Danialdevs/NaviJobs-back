193c56cb28524095e071e96a7f67b1365314a20e
Haten00 <johanworkofficial@mail.ru>
2024-07-04 10:00:26 +0600

    add changes api.php

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\User\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response as HttpResponse; // Используем алиас для разрешения констант
+
+class UserController extends Controller
+{
+    public function login(LoginRequest $request): JsonResponse
+    {
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        /** @var User|null $user */
+        $user = Auth::guard('web')->user();
+
+        if (!$user || !Auth::guard('web')->attempt(['email' => $email, 'password' => $password])) {
+            return response()->json([
+                'message' => 'Неверный логин или пароль',
+            ], HttpResponse::HTTP_UNAUTHORIZED);
+        }
+
+        $token = $user->createToken('login');
+
+        $user->update(['api_token' => $token->plainTextToken]);
+
+        return response()->json([
+            'message' => 'Успешный вход в систему',
+        ], HttpResponse::HTTP_OK);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,31 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken('token-name')->plainTextToken;
+});
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
