849633507c8ace2ab41b99678529588f18f98cc6
Haten00 <johanworkofficial@mail.ru>
2024-07-05 11:58:33 +0600

    auth

--- database/migrations/2024_06_26_083300_create_applications_table.php
+++ database/migrations/2024_06_26_083300_create_applications_table.php
@@ -4,31 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateApplicationsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('applications', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('company_offices_id')->constrained('company_offices');
+            $table->foreignId('company_office_id')->constrained('company_offices');
             $table->foreignId('client_id')->constrained('clients');
             $table->foreignId('service_id')->constrained('services');
             $table->string('address');
-            $table->string('comment')->nullable();
-            $table->enum('status', ['done-worker', 'full-done', 'awaiting', 'canceled']);
-            $table->string('system_comment');
+            $table->text('description');
+            $table->string('status');
+            $table->text('system_comment')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('applications');
     }
-};
+}
+
